0115c8eaca1eaca52ec384bce5afbf37c03cbc85
Anna Bass <bass.anna@hotmail.com>
2017-04-16 22:02:14 +0300

    Closes #48

--- Fake E-Health/select_vw_PatsiendiVaade.php
+++ Fake E-Health/select_vw_PatsiendiVaade.php
@@ -25,8 +25,25 @@ if ($result->num_rows > 0) {
 		echo "<td>" . $row["Arsti_eesnimi"] . "</td>";
         echo "<td>" . $row["Arsti_perekonnanimi"] . "</td>";
         echo "<td>" . $row["Aeg"] . "</td>";
-        echo "<td>" . $row["Systoolne"] . "</td>";
-        echo "<td>" . $row["Diastoolne"] . "</td>";
+        
+		$green = "#ddf4d9";
+        $orange = "#ffebd1";
+        $red = "#ffd5ce";
+
+        $sys = $row["Systoolne"];
+        $style = "";
+        if ($sys < 120) $style = $green;
+        else if ($sys < 140) $style = $orange;
+        else $style = $red;
+        echo "<td style='background-color: " . $style . "'>" . $row["Systoolne"] . "</td>";
+
+        $dia = $row["Diastoolne"];
+        $style = "";
+        if ($dia < 80) $style = $green;
+        else if ($dia < 90) $style = $orange;
+        else $style = $red;
+        echo "<td style='background-color: " . $style . "'>" . $row["Diastoolne"] . "</td>";
+		
         echo "<td>" . $row["Pulss"] . "</td>";
         echo "</tr>";
     }
