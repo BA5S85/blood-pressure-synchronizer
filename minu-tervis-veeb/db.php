2dfd1c8b58db35da1b2fa332700a9ba97e75aa2a
Anna Bass <bass.anna@hotmail.com>
2017-05-02 14:33:20 +0300

    Fixes #55

--- minu-tervis-veeb/db.php
+++ minu-tervis-veeb/db.php
@@ -10,3 +10,8 @@ $conn = new mysqli($server, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SET time_zone = '+2:00';";
+if ($conn->query($sql) !== TRUE) {
+    echo "Could not set time zone: " . mysqli_error($conn) . "<br>";
+}
